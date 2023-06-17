fc4adff66b5fbd6dddec13eef9b071069c672a13
vivekdomadiya <vivekmdomadiya@gmail.com>
2023-06-17 11:06:03 +0530

    added clear button and change on paste event

--- js/main.js
+++ js/main.js
@@ -2,22 +2,43 @@ var codeInput = $("#code");
 var phoneInput = $("#phone");
 var textInput = $("#text");
 var sendBtn = $("#send");
+var clearBtn = $("#clear");
 var codeError = $("#code-error");
 var PhoneError = $("#phone-error");
+var numberOnly = $(".numberOnly");
 
 // set input to number only
-$(".numberOnly").keypress((e) => {
+numberOnly.keypress((e) => {
   if (e.keyCode < 48 || e.keyCode > 57) {
     e.preventDefault();
     return false;
   }
 });
-$(".numberOnly").on("paste", (e) => {
-  var value = e.originalEvent.clipboardData.getData("Text");
-  if (value.match(/[^\d]/)) {
-    e.preventDefault();
-    return false;
+
+// phone number input paste validation
+phoneInput.on("paste", (e) => {
+  // check clipboardData has character except +, space and digits
+  var TextValue = e.originalEvent.clipboardData.getData("Text");
+  value = TextValue.replace(/\D/g, "");
+
+  if (TextValue.startsWith("+")) {
+    value = "+" + value;
   }
+
+  console.log(value);
+
+  // if value length is greater than 10 then set phoneNo to last 10 digit of value
+  if (value.length > 10) {
+    var code = value.substring(0, value.length - 10);
+    codeInput.val(code);
+
+    value = value.substring(value.length - 10, value.length);
+  }
+
+  phoneInput.val(value);
+
+  e.preventDefault();
+  return false;
 });
 
 // fix + sign to code
@@ -48,7 +69,6 @@ function validPhoneNo() {
 }
 
 // check valid on blur
-
 codeInput.on("blur", validCode);
 phoneInput.on("blur", validPhoneNo);
 
@@ -60,9 +80,18 @@ sendBtn.click((e) => {
   var phoneNo = phoneInput.val();
   var text = textInput.val();
   text = encodeURIComponent(text);
-  console.log(text);
-  //   return false;
+
   var phone = code + phoneNo;
   var url = "https://api.whatsapp.com/send?phone=" + phone + "&text=" + text;
   sendBtn.attr("href", url);
+});
+
+clearBtn.click((e) => {
+  codeInput.val("+91");
+  phoneInput.val("");
+  textInput.val("");
+  codeError.text("");
+  PhoneError.text("");
+  sendBtn.attr("href", "");
+  return false;
 });
